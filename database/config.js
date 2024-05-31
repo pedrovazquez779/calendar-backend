b19d53d92b45e8f966f6813a3af4e6107e8fb674
Pedro Vazquez <psv779@gmail.com>
2024-05-31 15:24:44 -0300

    Removed log

--- database/config.js
+++ database/config.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        console.log("ENV VAR: " + process.env.DB_CNN);
         await mongoose.connect(process.env.DB_CNN);
         console.log('DB Online');
     } catch (e) {
